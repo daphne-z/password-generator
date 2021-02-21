50b2acbd3291c85fb16d53c298e97f49291fd747
Daphne Zaborowski <dezaborowski@gmail.com>
2021-02-21 17:02:25 -0600

    updates

--- script.js
+++ script.js
@@ -1,48 +1,52 @@
-function enterCriteria() {
-  // Ask user for their password parameters (code based on student mini project solution)
-  var length = window.prompt("Enter a password length (8 to 128 characters)","");
-  var lowercasePref = window.prompt("Would you like to include lowercase letters in the password? Enter Y or N","");
-  var uppercasePref = window.prompt("Would you like to include uppercase letters in the password? Enter Y or N","");
-  var numbersPref = window.prompt("Would you like to include numbers in the password? Enter Y or N","");
-  var specialCharactersPref = window.prompt("Would you like to include special characters in the password? Enter Y or N");
+//Create global variables
+var length;
+var lowercasePref;
+var uppercasePref;
+var numbersPref;
+var specialCharacterPref;
+
+//Create arrays for password generation
+let lowercase = [
+  "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+];
+let uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
+];
+let number = [1, 2, 3, 4, 5, 6, 7, 8, 9,
+];
+let specialCharacter = [
+  "!", "@", "#", "$", "%", "^", "&", "*", "'", "(", ")", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "[", "\\", "]", "_",  "`",  "{", "|", "}",  "~",
+];
+let masterArray = [];
   
-  // If user pressed Cancel, immediately end function (code based on student mini project solution)
-  if (!length) {
-    return;
-    }
-    else if (!lowercasePref) {
-      return;
-    }
-    else if (!uppercasePref) {
-      return;
-    }
-    else if (!numbersPref) {
-      return;
-    }
-    else if (!specialCharactersPref) {
-      return;
-    }
-}
+//onclick --> initiate function that lets the user enter their data
+document.getElementById("generate").addEventListener("click", function enterCriteria (length) {
+var length = window.prompt("Enter a password length (8 to 128 characters)","");
+if (8 > length || length > 128) {window.alert ("Your password length doesn't meet the right criteria!")
+return};
+var lowercasePref = window.confirm("Would you like to include lowercase letters in the password?","");
+var uppercasePref = window.confirm("Would you like to include uppercase letters in the password?","");
+var numbersPref = window.confirm("Would you like to include numbers in the password?","");
+var specialCharacterPref = window.confirm("Would you like to include special characters in the password?");
+});
 
-// Convert to uppercase to make comparisons easier (code based on student mini project solution)
-  lowercasePref = lowercasePref.toUpperCase();
-  uppercasePref = uppercasePref.toUpperCase();
-  numbersPref = numbersPref.toUpperCase();
-  specialCharactersPref = specialCharactersPref.toUpperCase();
-
-//set parameters for random strings (used Travesty Media for guideance on this approach)
-  const lowercase = "abcdefghijklmnopqrstuvwxyz";
-  const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  const number = "1234567890";
-  const specialChar = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-
-//generate the password
-//function generatePassword() {
-//for ( var i = 0; i < length; i++ ) {
-//}}
+//generate the password based on the preferences the user entered
+function generatePassword(lowercasePref, uppercasePref, numbersPref, specialCharacterPref) {
+  if (lowercasePref == true){
+  then masterArray.concat(lowercase);
+  }
+  else if (uppercasePref == true){
+    then masterArray.concat(uppercase);
+  }
+  else if (numbersPref == true){
+  then masterArray.concat(number);
+  }
+  else if (specialCharacterPref == true){
+  then masterArray.concat(speciaCharacter);
+  }
+  var password = Math.floor(Math.random)*masterArray.length;
+};
 
 // Write password to the #password input (code provided)
-//function writePassword() {
-//var password = generatePassword();
-//var passwordText = document.querySelector("#password");
-//}
+function writePassword(password) {
+var password = document.querySelector("#password")
+};
