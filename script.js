600a4da09539fce52e1f8fd964fc28ccadbdfad7
Daphne Zaborowski <dezaborowski@gmail.com>
2021-02-22 11:51:37 -0600

    updating

--- script.js
+++ script.js
@@ -13,30 +13,23 @@ let masterArray = [];
 
 
 //Step 2: Onclick run the generate password function which creates the password string (Triology was used for guidance for this coding step)
-document.getElementById("generate").addEventListener("click", function generatePassword () {
+document.getElementById("generate").addEventListener("click", function generatePassword (lowercase, uppercase, number, specialCharacter, masterArray) {
 
 //Step 2.1: Have the user enter their preference data
 
-let lengthPref=window.prompt("Enter a password length (8 to 128 characters)","");
+let lengthPref=window.prompt("Enter a password length (8 to 128 characters) or hit enter to accept the default", 15);
 
 //Step 2.2: Validate that length fits the parameters
 if (8 > lengthPref || lengthPref > 128) {window.alert ("Your password length doesn't meet the right criteria!")
-return};
+return false};
 
 let lowercasePref = window.confirm("Would you like to include lowercase letters in the password?","");
 let uppercasePref = window.confirm("Would you like to include uppercase letters in the password?","");
 let numbersPref = window.confirm("Would you like to include numbers in the password?","");
 let specialCharacterPref = window.confirm("Would you like to include special characters in the password?");
 
-let finalPass= "";
+//Step 2.3: Populate the master array with concat
 
-//Step 2.3: Run loop that generates password string based on preferences
-
-for (let i = 0; i < lengthPref; i++) {
-
-let randomIndex = Math.floor(math.random()*masterArray.length)
-finalPass += masterArray[randomIndex]
-  
 if (lowercasePref == true){
 masterArray=masterArray.concat(lowercase);
   }
@@ -51,11 +44,18 @@ masterArray=masterArray.concat(number);
 
 else if (specialCharacterPref == true){
  masterArray=masterArray.concat(specialCharacter);
- }
-
 }
-return finalPass;
 
+//Step 2.4: Randomize from the masterArray, loop through the number of times in the  password length pref
+
+var finalPass = "";
+
+for (let i = 0; i < lengthPref; i++){
+  finalPass = finalPass + masterArray.charAt(Math.floor(Math.random() * Math.floor(masterArray.length-1)));
+}
+
+//step 2.5//return the final password
+return finalPass;
 });
 
 // Step 3: Write password to the #password input (Trilogy was used for guidance for this coding step)
